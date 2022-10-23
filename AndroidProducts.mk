4d94eb65b52cae1a6a9b530993662179df485075
RaidenIshigami <contact.raidenishi69@gmail.com>
2022-10-23 08:25:03 +0000

    Mi439: yaap bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_Mi439.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Mi439-user \
-    lineage_Mi439-userdebug \
-    lineage_Mi439-eng
+    yaap_Mi439-user \
+    yaap_Mi439-userdebug \
+    yaap_Mi439-eng
