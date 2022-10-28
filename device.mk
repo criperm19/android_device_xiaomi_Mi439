405b1fea1fc8ddd7185531fa3a9b476e39217cf8
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-10-28 09:49:06 -0300

    Mi439: use kernel prebuilt

--- device.mk
+++ device.mk
@@ -60,6 +60,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Kernel
+PRODUCT_COPY_FILES += \
+    device/qualcomm/kernel-sdm439_64/Image.gz-dtb:kernel
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
