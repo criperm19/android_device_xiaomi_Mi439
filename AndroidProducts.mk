e445cc7497c356fa2c67b22375a0381c4bffb765
Raiden Ishigami <contact.raidenishi69@gmail.com>
2023-02-02 23:17:44 -0300

    Mi439: AwakenOS bringup
    Signed-off-by: Raiden Ishigami <contact.raidenishi69@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Mi439.mk \
-    $(LOCAL_DIR)/lineage_Mi439_4_19.mk
+    $(LOCAL_DIR)/awaken_Mi439.mk \
+    $(LOCAL_DIR)/awaken_Mi439_4_19.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Mi439-user \
-    lineage_Mi439-userdebug \
-    lineage_Mi439-eng \
-    lineage_Mi439_4_19-user \
-    lineage_Mi439_4_19-userdebug \
-    lineage_Mi439_4_19-eng
+    awaken_Mi439-user \
+    awaken_Mi439-userdebug \
+    awaken_Mi439-eng \
+    awaken_Mi439_4_19-user \
+    awaken_Mi439_4_19-userdebug \
+    awaken_Mi439_4_19-eng
