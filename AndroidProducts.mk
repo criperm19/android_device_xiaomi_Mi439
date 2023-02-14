90bb527771247d6fcbce699ce35c93d141050672
Raiden Ishigami <contact.raidenishi69@gmail.com>
2023-02-14 06:38:54 -0300

    Mi439: xdroid bringup
    Signed-off-by: Raiden Ishigami <contact.raidenishi69@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,13 +5,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_Mi439.mk \
-    $(LOCAL_DIR)/cherish_Mi439_4_19.mk
+    $(LOCAL_DIR)/xdroid_Mi439.mk \
+    $(LOCAL_DIR)/xdroid_Mi439_4_19.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_Mi439-user \
-    cherish_Mi439-userdebug \
-    cherish_Mi439-eng \
-    cherish_Mi439_4_19-user \
-    cherish_Mi439_4_19-userdebug \
-    cherish_Mi439_4_19-eng
+    xdroid_Mi439-user \
+    xdroid_Mi439-userdebug \
+    xdroid_Mi439-eng \
+    xdroid_Mi439_4_19-user \
+    xdroid_Mi439_4_19-userdebug \
+    xdroid_Mi439_4_19-eng
--- xdroid_Mi439.mk
+++ xdroid_Mi439.mk
@@ -9,28 +9,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common CherishOS stuff.
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_BUILD_GRAPHENEOS_CAMERA := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_BOOT_ANIMATION_RES := 720
-TARGET_ENABLE_BLUR := true
-
-# Vanilla build stuff.
-CHERISH_VANILLA := true
+# Inherit some common xdroid stuff.
+$(call inherit-product, vendor/xdroid/config/common_full_phone.mk)
 
 # Additional stuff
 SELINUX_IGNORE_NEVERALLOWS =: true
 ALLOW_MISSING_DEPENDENCIES =: true
 RELAX_USES_LIBRARY_CHECK =: true
 PRODUCT_BROKEN_VERIFY_USES_LIBRARIES =: true
-
-# Maintainer stuff.
-CHERISH_BUILD_TYPE := OFFICIAL
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=RaidenIshigami
 
 # Kernel
 TARGET_KERNEL_VERSION := 4.9
@@ -45,7 +31,7 @@ PRODUCT_PACKAGES += \
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := Mi439
-PRODUCT_NAME := cherish_Mi439
+PRODUCT_NAME := xdroid_Mi439
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SDM439
